94181227f9f82417b435065ed3fe74c4d3f9dd73
Matti Virolainen <mvirolai@gmail.com>
2026-03-20 21:25:35 +0200

    Allow pasting with Cmd-V

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Terminal mode: Cmd-V to paste from system clipboard (Neovide)
+vim.keymap.set("t", "<D-v>", function()
+  vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+end, { noremap = true })
